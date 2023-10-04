dc6ee31e7279a5f7afae6d1507e7990fd86d35b3
Lorenzo_Costabile <lorenzocostabile@grupo-sic.com>
2023-10-04 10:34:51 +0200

    paquete

---
+++ es/components/Oval/index.js
@@ -0,0 +1,26 @@
+var _templateObject;
+function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+function _taggedTemplateLiteralLoose(strings, raw) { if (!raw) { raw = strings.slice(0); } strings.raw = raw; return strings; }
+import React from 'react';
+import styled from 'styled-components';
+var Container = styled.div(_templateObject || (_templateObject = _taggedTemplateLiteralLoose(["\n  border: dashed 2px black;\n  border-radius: 100%;\n  box-shadow: 0px 0px 1px 1px white inset;\n  box-sizing: border-box;\n  transition: box-shadow 0.21s ease-in-out;\n"])));
+function Oval(props) {
+  var geometry = props.annotation.geometry;
+  if (!geometry) return null;
+  return /*#__PURE__*/React.createElement(Container, {
+    className: props.className,
+    style: _extends({
+      position: 'absolute',
+      left: geometry.x + "%",
+      top: geometry.y + "%",
+      height: geometry.height + "%",
+      width: geometry.width + "%",
+      boxShadow: props.active && '0 0 1px 1px yellow inset'
+    }, props.style)
+  });
+}
+Oval.defaultProps = {
+  className: '',
+  style: {}
+};
+export default Oval;
